d513179eb69240c3d7a410da172cd58be2e35a93
Zhansaya <zhansayanur19999@gmail.com>
2025-06-02 14:46:05 +0500

    first commit

---
+++ app/onboarding.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text>Onboarding</Text>
+    </View>
+  )
+}
+
+export default Onboarding
+
+const styles = StyleSheet.create({})
